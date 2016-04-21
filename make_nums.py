e342c860ddfc1d17cdbde5cf4dcb790993231757
willdeuschle <wjdeuschle@gmail.com>
2016-04-20 23:14:06 -0400

    rand nums

--- make_nums.py
+++ make_nums.py
@@ -1,1 +1,11 @@
+import numpy as np
+import sys
 
+# usage: python gen_numbers.py n
+# creates nums.txt with appropriate numbers
+
+def gen_numbers():
+    for i in range(0,100):
+        print np.random.random_integers(1, 10**12)
+
+gen_numbers()
