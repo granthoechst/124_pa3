75bc731c8495a41ff1d1af54317b9130b454dd49
granthoechst <granthoechst@college.harvard.edu>
2016-04-24 15:22:18 -0400

    data collection

---
+++ data_collection.sh
@@ -0,0 +1,201 @@
+#!/bin/bash
+echo "kk 1:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 2:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 3:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 4:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 5:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 6:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 7:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 8:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 9:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 10:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 11:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 12:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 13:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 14:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 15:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 16:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 17:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 18:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 19:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 20:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 21:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 22:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 23:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 24:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 25:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 26:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 27:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 28:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 29:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 30:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 31:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 32:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 33:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 34:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 35:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 36:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 37:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 38:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 39:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 40:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 41:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 42:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 43:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 44:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 45:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 46:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 47:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 48:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 49:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
+echo "kk 50:"
+python make_nums.py > nums.txt
+python karmarkarkarp.py nums.txt
+echo "--"
